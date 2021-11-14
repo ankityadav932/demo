1bbe8b00d1c1de580a01c899cf518e5a3e3b2c08
Ankit Yadav <ankitrao.a1@gmail.com>
2021-11-14 17:53:33 +0530

    test commit

---
+++ app.js
@@ -0,0 +1,32 @@
+document.querySelector('button#open-media').addEventListener('click', () => 
+{
+	if (hasUserMedia()) {callUserMedia()}
+	connect();	
+});
+
+// Define Variables ---------------------
+var localstream = null;
+var remotestream = null;
+
+let hasUserMedia = () => 
+{
+	// Check for Media --------------------------
+	return (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia) ? true : false;
+};
+
+let callUserMedia = () => 
+{
+	if (hasUserMedia()) 
+	{
+		// Get Supported Browser Stream -------------
+		navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia; 
+
+		// Access User Media ------------------------
+		navigator.getUserMedia({video: true, audio: true}, (stream) => {
+			var conn = new RTCPeerConnection();
+			conn.addStream(stream);
+
+			document.querySelector('#local-frame').srcObject = stream;
+		}, (err) => {console.log(err);});
+	}
+};
